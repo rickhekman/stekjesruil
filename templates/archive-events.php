1ca5b99940aadbc6f1818d031a55715412cd8171
Rick Hekman <info@rickhekman.com>
2023-03-16 17:29:22 +0100

    Add feature event custom post type

---
+++ templates/archive-events.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Template Name: Archive events
+*/
+?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <?php
+
+    $block_header = do_blocks( '
+    <!-- wp:template-part {"slug":"header","theme":"stekjesruil"} /-->
+    '
+    );
+
+    $block_footer = do_blocks( '
+    <!-- wp:template-part {"slug":"footer","theme":"stekjesruil"} /-->
+    '
+    );
+
+    ?>
+
+    <?php wp_head(); ?>
+  </head>
+
+  <body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+      <div class="wp-site-blocks">
+        <?php echo $block_header ?>
+        <!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
+        <main class="wp-block-group" style="margin-top:var(--wp--preset--spacing--70);margin-bottom:var(--wp--preset--spacing--70)">
+          <!-- wp:group {"layout":{"type":"constrained"}} -->
+          <div class="wp-block-group">
+
+            <div class="events-query">
+              <ul class="events">
+                <?php
+                  $allEvents = new WP_Query(array(
+                    'post_per_page' => 6,
+                    'post_type' => 'event'
+                  ));
+
+                  while($allEvents -> have_posts()) {
+                    $allEvents -> the_post(); ?>
+                      <li class="event__item">
+                        <figure class="event__image">
+                            <?php the_post_thumbnail( 'large' );  ?>
+                        </figure>
+                        <div class="event__description">
+                          <?php the_title( sprintf( '<h2 class="event__title"><a href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+                          <?php the_excerpt(); ?>
+                        </div>
+                      </li>
+                  <?php }
+                ?>
+              </ul>
+            </div>
+
+          </div>
+          <!-- /wp:group -->
+
+        </main>
+        <!-- /wp:group -->
+
+        <?php echo $block_footer ?>
+      </div>
+  </body>
+</html>
